7b731ac684cdf4a53f6002f38a413968925a7ed8
Francis Lavalliere <support@acentera.com>
2014-10-08 19:47:25 -0400

    fix

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -3,7 +3,7 @@
 
 # --- !Ups
 
-CREATE INDEX `IDX_ROLE` ON PROJECT_USER_ROLES ( `USER_ID`,`PROJECT_TAGS_ID`,`PROJECT_ID`);
+CREATE INDEX `IDX_ROLE` ON PROJECT_USER_ROLES ( `USER_ID`,`PROJECT_TAGS_ID`);
 
 create index idx_unique_active_user on `project_user` (`project_id`,`user_id`,`disable_date`);
 
